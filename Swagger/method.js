b0dd082c5a9e2e8aca05ef8c7b1dce46b56c34c0
magyarcs <magyarcs@kovax.hu>
2019-04-15 14:04:49 +0200

    Very small changes Parms changed to params Name and description in method.js needs to be changed every time an OK code was received with the same species name already, otherwise ==> 404...

--- Swagger/method.js
+++ Swagger/method.js
@@ -1,12 +1,12 @@
 function method1(){
 var url = "http://81.2.241.234:8080/species";
-var parms = 'name=somethingsomething&desc=descriptionsText';
+var params = 'name=somethingsomething123&desc=descriptionTest123';
 
 var request = new XMLHttpRequest();
 request.open('POST', url, true);
 request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 request.addEventListener('load', reqListener);
-request.send(parms);
+request.send(params);
 
 function reqListener(){document.body.innerHTML = request.responseText;}
 }
