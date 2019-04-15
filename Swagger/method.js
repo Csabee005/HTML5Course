7bf9d1e42d6b454ef516dfd957d05869e1469b94
magyarcs <magyarcs@kovax.hu>
2019-04-15 16:56:14 +0200

    Updated with POST modification ID not yet implemented, but works for the static one currently written as URL

--- Swagger/method.js
+++ Swagger/method.js
@@ -1,9 +1,29 @@
 function method1(){
 var url = "http://81.2.241.234:8080/species";
-var params = 'name=somethingsomething123&desc=descriptionTest123';
+
+var name = document.getElementById("txtNameCreateHero").value;
+var description = document.getElementById("txtDescriptionCreateHero").value;
+var params = 'name='+name+'&desc='+description;
 
 var request = new XMLHttpRequest();
 request.open('POST', url, true);
+request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+request.addEventListener('load', reqListener);
+request.send(params);
+
+function reqListener(){document.body.innerHTML = request.responseText;}
+}
+
+function method2(){
+var url = "http://81.2.241.234:8080/species/1640";
+
+var name = document.getElementById("txtNameCreateHero").value;
+var description = document.getElementById("txtDescriptionCreateHero").value;
+var params = 'id=1640&';
+params = 'name='+name+'&desc='+description;
+//ID = 1640
+var request = new XMLHttpRequest();
+request.open('PUT', url, true);
 request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 request.addEventListener('load', reqListener);
 request.send(params);
