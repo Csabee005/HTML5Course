104dbf47025788deabaa0d88d8cc44ecd82aa4ae
Csabee005 <csabee005@gmail.com>
2019-04-16 22:48:41 +0200

    Implemented succes&failure response display Continued work on design

--- Swagger/method.js
+++ Swagger/method.js
@@ -11,7 +11,16 @@ request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 request.addEventListener('load', reqListener);
 request.send(params);
 
-function reqListener(){document.body.innerHTML = request.responseText;}
+function reqListener(){
+		if(request.status == 200){
+			failureText.innerHTML = "";
+			successText.innerHTML = "Specimen creation succesful with " + request.responseText + " as response.";
+		}
+		else{
+			successText.innerHTML = "";
+			failureText.innerHTML = "Failed to create specimen!";
+		}
+	}
 }
 
 function method2(){
@@ -29,4 +38,43 @@ request.addEventListener('load', reqListener);
 request.send(params);
 
 function reqListener(){document.body.innerHTML = request.responseText;}
+}
+
+function onCreate(){
+  $( function() {
+    $( "#dialog" ).dialog({
+      autoOpen: false,
+      modal: true,
+      close: closeFunction(),
+      show: {
+        effect: "scale",
+        duration: 250
+      },
+      hide: {
+        effect: "clip",
+        duration: 250
+      }
+    }).prev(".ui-dialog-titlebar").css("background","#e6ffff");
+ 	
+ 	$( "#dialog" ).dialog({
+  	minWidth: 700
+		});
+ 	//$( "#dialog" ).dialog( "option", "minHeight", 400 );
+	$( "#dialog" ).dialog({
+	  closeText: ""
+	});
+
+    $( "#opener" ).on( "click", function() {
+      $( "#dialog" ).dialog( "open" );
+    });
+  });
+}
+
+function closeFunction(){
+	var successText = document.getElementById("txtSuccessCreation");
+	var failureText = document.getElementById("txtFailureCreation");
+	document.getElementById("txtNameCreateHero").value = "";
+	document.getElementById("txtDescriptionCreateHero").value = "";
+	successText.innerHTML = "";
+	failureText.innerHTML = "";
 }
