5008c3ed60111fadaf0ed738c706ea7ec8476a8e
Magyar Csaba <csabee005@gmail.com>
2019-05-27 16:05:21 +0200

    Continued work on species page Created fixed top navigation bar Color palette now fixed for every dialog

--- Swagger/method.js
+++ Swagger/method.js
@@ -237,18 +237,3 @@ function onRemovalDialog(button) {
         $("#deletionDialog").dialog("close");
     }
 }
-
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(135, 147, 136,100)";
-}
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = getComputedStyle(document.documentElement)
-        .getPropertyValue('--secondary-color-nyanza-light');
-}
